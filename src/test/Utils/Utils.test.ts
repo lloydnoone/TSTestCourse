4ac9c4c223e0fd36205b9553c9b4af31be5a410a
lloydnoone <lloydnoone92@gmail.com>
2020-12-21 15:07:06 +0000

    add stubs

--- src/test/Utils/Utils.test.ts
+++ src/test/Utils/Utils.test.ts
@@ -1,29 +1,31 @@
+import { IncomingMessage } from 'http'
 import { Utils } from '../../app/Utils/Utils'
-import { IncomingMessage } from 'http';
 
 describe('Utils test suite', () => {
+  test('getRequestPath valid request', () => {
+    const request = {
+      url: 'http://localhost:8080/login'
+    } as IncomingMessage
 
-    test('getRequestPath valid request', () => {
-        const request = {
-            url: 'http://localhost:8080/login'
-        } as IncomingMessage;
-        const resultPath = Utils.getRequestBasePath(request);
-        expect(resultPath).toBe('login');
-    });
+    const resultPath = Utils.getRequestBasePath(request)
+    expect(resultPath).toBe('login')
+  })
 
-    test('getRequestPath with no path name', () => {
-        const request = {
-            url: 'http://localhost:8080/'
-        } as IncomingMessage;
-        const resultPath = Utils.getRequestBasePath(request);
-        expect(resultPath).toBeFalsy();
-    });
+  test('getRequestPath request path with no path name', () => {
+    const request = {
+      url: 'http://localhost:8080/'
+    } as IncomingMessage
 
-    test('getRequestPath with no path name', () => {
-        const request = {
-            url: ''
-        } as IncomingMessage;
-        const resultPath = Utils.getRequestBasePath(request);
-        expect(resultPath).toBeFalsy();
-    });
-});
+    const resultPath = Utils.getRequestBasePath(request)
+    expect(resultPath).toBeFalsy()
+  })
+
+  test('getRequestPath request path with empty string', () => {
+    const request = {
+      url: 'http://localhost:8080/'
+    } as IncomingMessage
+
+    const resultPath = Utils.getRequestBasePath(request)
+    expect(resultPath).toBeFalsy()
+  })
+})
