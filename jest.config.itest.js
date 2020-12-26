63adaa1d32fba1fa0cb301418c2195c6d3928e26
lloydnoone <lloydnoone92@gmail.com>
2020-12-26 15:10:58 +0000

    add integration tests

--- jest.config.itest.js
+++ jest.config.itest.js
@@ -1,10 +1,10 @@
 module.exports = {
-    roots: ['<rootDir>/src/itest'],
-    transform: {
-        '^.+\\.tsx?$': 'ts-jest'
-    },
-    testRegex: '(/__test__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    verbose: true,
-    testEnvironment: "node"
+  roots: ['<rootDir>/src/itest'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  testRegex: '(/__test__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  verbose: true,
+  testEnvironment: 'node'
 }
